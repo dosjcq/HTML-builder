0811d22146c44a403d5369989b02b3c3a787fbaa
dosjcq <nagibin.artyom1999@gmail.com>
2021-10-31 13:02:19 +0300

    feat: solve task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+let fs =  require('fs')
+const path = require('path')
+
+
+getAllFiles('03-files-in-folder/secret-folder');
+
+function getAllFiles(folderDir) {
+  fs.readdir(folderDir, (err, files) => {
+    if (err) throw err;
+  
+    // console.log(files);
+  
+    for (let file of files) {
+      fs.stat(folderDir + '/' + file, (err, stats) => {
+        if (err) throw err;
+        
+        if(!stats.isDirectory()) {
+          console.log(path.basename(folderDir + '/' + file, path.extname(folderDir + '/' + file)) + ' - ' + path.extname(folderDir + '/' + file).substr(1) + ' - ' + `${stats.size / 1024}kb`);
+          
+        } else {
+          getAllFiles(folderDir + '/' + file)
+        }
+      })
+    }
+  })
+}
+
+
