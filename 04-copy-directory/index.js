96bd378af93cbf0f295268a04eeb9c91b199eba3
dosjcq <nagibin.artyom1999@gmail.com>
2021-11-02 19:17:50 +0300

    feat: start task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+let fs = require("fs");
 
+fs.mkdir("04-copy-directory/files-copy", (err) => {
+  if (err) {
+    fs.rmdir("04-copy-directory/files-copy", (err) => {
+      if (err) throw err;
+    });
+  }
+  fs.mkdir("04-copy-directory/files-copy", (err) => {
+    if (err) throw err;
+  });
+});
+
+getAllFiles("04-copy-directory/files");
+
+function getAllFiles(folderDir) {
+  fs.readdir(folderDir, (err, files) => {
+    if (err) throw err;
+
+    // console.log(files);
+
+    for (let file of files) {
+      // fs.stat(folderDir + "/" + file, (err, stats) => {
+      //   if (err) throw err;
+      // });
+      fs.copyFile(
+        `04-copy-directory/files/${file}`,
+        `04-copy-directory/files-copy/${file}`,
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    }
+  });
+}
